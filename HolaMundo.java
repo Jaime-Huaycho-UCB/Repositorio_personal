492f2986f6a5d5691baad62d27a4423084899a5a
Jaime-Huaycho-Ubuntu <huaychojaime@gmail.com>
2024-05-19 02:11:09 -0400

    act

--- HolaMundo.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class HolaMundo{
-    public static void main(String[] args) {
-        int n = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un numero"));
-        JOptionPane.showMessageDialog(null,"tu numero es "+n);
-    }
-}
