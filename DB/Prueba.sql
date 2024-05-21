33f60ae532165263d3eb5f9e87464ba607f58198
Jaime-Huaycho-MacBook <huaychojaime@gmail.com>
2024-05-20 22:02:54 -0400

    Tabla persona

--- DB/Prueba.sql
+++ DB/Prueba.sql
@@ -1,14 +1,21 @@
 create database Prueba;
 use Prueba;
 
-create table Usuario(
-	id_usuario numeric(10,0) not null,
+create table Persona(
+	id int not null,
     nombre varchar(20) not null,
-    apellido varchar(20) not null,
-    edad int not null,
-    primary key(id_usuario)
+    paterno varchar(20) not null,
+    materno varchar(20) not null,
+    fecha_nacimiento date not null,
+	numero_celular numeric(8,0) not null,
+    primary key (id)
 );
 
-select * from Usuario;
-insert into Usuario (id_usuario,nombre,apellido,edad) 
-values (8343043,"jaime","huaycho",19);
+insert into Persona (id,nombre,paterno,materno,fecha_nacimiento,numero_celular) 
+values(8343043,"Jaime Ignacio","Huaycho","Clavel","2005-04-16",64177602);
+
+update Persona set fecha_nacimiento="2005-04-16" where id=8343043;
+
+delete from Persona where id=8343043;
+
+select * from Persona;
